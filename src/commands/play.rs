738526a6bc17ed1ba78b8d0c0713f64d918fdf35
Senre <--global>
2020-05-16 09:43:21 +0800

    Added ConnectFour Initialization

---
+++ src/commands/play.rs
@@ -0,0 +1,96 @@
+use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+use serenity::model::channel::ReactionType;
+
+#[command]
+fn c4(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let mut c4 = msg
+        .channel_id
+        .say(&ctx.http, "Loading <a:loading:617628744512700447>")
+        .unwrap();
+
+    add_react(ctx, &c4);
+
+    c4.edit(&ctx, |m| {
+        m.content("").embed(|e| {
+            e.author(|a| {
+                a.name("Connect Four")
+                    .icon_url(&ctx.cache.read().user.face())
+            })
+            .color((255, 255, 0))
+            .field("Awaiting for player", "Turns #", false)
+            .image("https://i.imgur.com/Pnfyxmh.png")
+        })
+    })
+    .unwrap();
+
+    Ok(())
+}
+
+fn add_react(ctx: &mut Context, msg: &Message) {
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304998428672010),
+            name: Some("1_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304999938359306),
+            name: Some("2_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304999883833347),
+            name: Some("3_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304999057817601),
+            name: Some("4_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304999171063809),
+            name: Some("5_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304999451951105),
+            name: Some("6_".to_string()),
+        },
+    )
+    .unwrap();
+    msg.react(
+        &ctx,
+        ReactionType::Custom {
+            animated: false,
+            id: EmojiId(621304998919274506),
+            name: Some("7_".to_string()),
+        },
+    )
+    .unwrap();
+}
