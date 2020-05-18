7a765f26fff112df9d85a759c5233c2642f60bba
Senre <--global>
2020-05-18 16:37:46 +0800

    Made in separate file

---
+++ src/core/shardmanager_container.rs
@@ -0,0 +1,11 @@
+use serenity::client::bridge::gateway::ShardManager;
+
+use serenity::prelude::{Mutex, TypeMapKey};
+
+use std::sync::Arc;
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
