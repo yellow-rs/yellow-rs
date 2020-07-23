1fec42227ee47c4e284fb4e13ffe8de461ebd5b4
Jonathan <30177086+MonliH@users.noreply.github.com>
2020-07-23 14:08:50 -0400

    Help command description for eval

--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -6,6 +6,9 @@ use crate::core::eval;
 
 #[command]
 #[min_args(1)]
+#[description(r#"Evaluates a mathematical expression.
+Infix operators available: `+` (addition), `-` (subtraction), `/` (division), `//` (integer division), `*` (multiplication), `**` (exponent), `as` (conversion).
+Prefix operators: `-` (negate), `+` (absolute)."#)]
 async fn eval(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let result = match eval::exec(&args.rest()[..]) {
         Ok(val) => val,
