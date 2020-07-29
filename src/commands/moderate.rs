40a8979767ba0ad67b660d3e095abfa7217a9c58
Senre <jesuisutile0@gmail.com>
2020-07-29 15:21:12 +0800

    remove unheard-of commands

--- src/commands/moderate.rs
+++ src/commands/moderate.rs
@@ -6,18 +6,6 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
-#[description("Flags a user, making them unable to embed links and send any media.")]
-async fn flag(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    Ok(())
-}
-
-#[command]
-#[description("")]
-async fn silent(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    Ok(())
-}
-
-#[command]
 #[description("Kicks out a guild member.")]
 async fn kick(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     Ok(())
