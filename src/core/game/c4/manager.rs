56a7010829a4b6bf44c6ddd699baeb4322376e6b
Senre <jesuisutile0@gmail.com>
2020-08-17 10:16:13 +0800

    Remove RwLocks

--- src/core/game/c4/manager.rs
+++ src/core/game/c4/manager.rs
@@ -1,6 +1,11 @@
-pub type C4Manager = HashMap<u64, Arc<RwLock<C4Instance>>>;
+use crate::core::game::c4::{board_trait::C4Board, instance::C4Instance};
+use serenity::prelude::{RwLock, TypeMapKey};
+use std::{collections::HashMap, sync::Arc};
+
+pub type C4Manager = HashMap<u64, C4Instance<dyn C4Board>>;
 
 pub struct C4ManagerContainer;
+
 impl TypeMapKey for C4ManagerContainer {
     type Value = Arc<RwLock<C4Manager>>;
 }
