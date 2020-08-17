f85e431f48a90978da1f8b91846a4a4bd2dca671
Senre <jesuisutile0@gmail.com>
2020-08-17 10:17:42 +0800

    Dynamic Board Size

--- src/core/game/c4/instance.rs
+++ src/core/game/c4/instance.rs
@@ -1,10 +1,25 @@
-pub struct C4Instance<Board: BoardPlayable> {
-    msg: Message,      // Message to manipulate
-    http: Arc<Http>,   // Http object to interact with message
-    board_data: Board, // Board data wrapper
-    board_canvas: ImageSurfaceWrapper,
-    players_pair: [User; 2],
-    avatars: [ImageSurfaceWrapper; 2],
+use crate::core::game::c4::board_trait::C4Board;
+use serenity::{http::Http, model::channel::Message};
+use std::sync::Arc;
+
+pub struct C4Instance {
+    msg: Message,                 // Message to manipulate
+    http: Arc<Http>,              // Http object to interact with message
+    board_data: Box<dyn C4Board>, // Board data wrapper
+    players_pair: [u64; 2],
     turns: u8,
     over: bool,
+}
+
+impl C4Instance {
+    pub fn new(msg: Message, http: Arc<Http>, board_data: dyn C4Board) -> Self {
+        C4Instance {
+            msg,
+            http,
+            board_data,
+            players_pair: [0; 2],
+            turns: 1,
+            over: false,
+        }
+    }
 }
